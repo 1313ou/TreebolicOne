14bce1e36fb3fc0075a84977c7c64911f76e8d8c
1313ou <1313ou@gmail.com>
2023-11-25 18:40:07 +0100

    fixes artwork generation

--- treebolicOwlProvider/artwork/make-artwork.sh
+++ treebolicOwlProvider/artwork/make-artwork.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-source "../../../make-artwork-lib.sh"
+source "../../../lib-artwork.sh"
 
 icons="*.svg"
 root="root*.svg"
 
-make_res "${icons}" 48
-make_res "${root}" 64
+#make_res "${icons}" 48
+#make_res "${root}" 64
+
+make_icon "${icons}" 32 "${dirresources}/treebolic/provider/owl/images"
+make_icon "${root}" 48 "${dirresources}/treebolic/provider/owl/images"
 
